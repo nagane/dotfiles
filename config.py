2dbb71266e341e9cbd28e1b717588fe0284bb261
nagane <naganeys@gmail.com>
2017-03-28 13:37:10 +0900

    add keyhac config

---
+++ config.py
@@ -0,0 +1,16 @@
+﻿import sys
+import os
+import datetime
+import subprocess
+
+from keyhac import *
+
+
+def configure(keymap):
+
+    # Vim key bind
+    keymap_global = keymap.defineWindowKeymap()
+    keymap_global["LAlt-H"] = "Left"
+    keymap_global["LAlt-J"] = "Down"
+    keymap_global["LAlt-K"] = "Up"
+    keymap_global["LAlt-L"] = "Right"
